a983785854b814438d5d176ea9004989642d33c3
AnotherJelly <144315788+AnotherJelly@users.noreply.github.com>
2024-07-01 14:17:27 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="header-block">
+    <a href="https://pari.ru/" target="_blank">
+        <img src="/wp-content/uploads/2024/06/logo.png" class="header-logo" alt="">
+    </a>
+    <div class="header-block-right">
+        <div class="header-text">
+            <span>фрибет</span>
+            <span>2024₽</span>
+            <div>новым<br>клиентам</div>
+        </div>
+        <a class="button-default" target="_blank" href="https://pari.ru/authProcess/registration/">
+            забирай
+        </a>
+    </div>
+</header>
